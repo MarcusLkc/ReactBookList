4b3ecbabdf6bac89dbd3e1c51feb5696b03ae48f
marcuslkc <marcuscrowder66@gmail.com>
2017-06-20 14:55:22 -0400

    Added BookReducer

---
+++ src/reducers/reducer_active_books.js
@@ -0,0 +1,7 @@
+// State argument is not application state, only the state
+// this reducer is resposible for
+
+export default function(state, action) {
+
+
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function (){
+	return [
+		{title: 'Javascript: TheGood Parts'},
+		{title: 'Harry Potter'},
+		{title: 'The Dark Tower'},
+		{title: 'Eloquent Ruby'},
+
+	]
+}
